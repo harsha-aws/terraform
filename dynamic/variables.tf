fbf5cce7521c74a6766df3277b8379391da2063a
harsha-aws <hv147369@gmail.com>
2024-09-22 01:45:16 +0530

    terraform

---
+++ dynamic/variables.tf
@@ -0,0 +1,30 @@
+variable "ingress_rules" {
+  default     = [
+    {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        idr_blocks = ["0.0.0.0/0"]
+        description = "opening 22"
+
+    },
+
+    {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        idr_blocks = ["0.0.0.0/0"]
+        description = "opening 80"
+
+    },
+
+    {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        idr_blocks = ["0.0.0.0/0"]
+        description = "opening 8080"
+
+    }
+  ]
+}
